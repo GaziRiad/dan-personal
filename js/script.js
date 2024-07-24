7479fae4047e8684d87b44f799ba8e4c0ede9ef6
GaziRiad <ouhibimustapha36@gmail.com>
2024-07-25 00:15:35 +0100

    made home page responsive

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// const btnNavEl = document.querySelectorAll(".humburger-menu");
+// const sideMenu = document.querySelector(".sidebar");
+
+// ///////////////////////////////////////////////////////////
+// // Make mobile navigation work
+
+// btnNavEl.forEach((btn) =>
+//   btn.addEventListener("click", function () {
+//     sideMenu.classList.toggle("nav-open");
+//     if (sideMenu.classList.contains("nav-open")) {
+//       document.documentElement.style.overflowY = "hidden";
+//     } else {
+//       document.documentElement.style.overflowY = "auto";
+//     }
+
+//     console.log("Humberger button click!");
+//   })
+// );
